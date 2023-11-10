0c338f3062365361f58a53a8f4c59f24b7d2652d
srg <skabassjob@gmail.com>
2023-11-10 15:54:19 +0400

    feat: button panel, select-input

--- src/script/utils.js
+++ src/script/utils.js
@@ -5,20 +5,90 @@ window.utils = {
   BREAKPOINT_TABLET: '(max-width: 1023px)',
 
   /**
-   * add/remove css class when click button
+   * add/remove css class of button
    * @param {HTMLButtonElement} btn
-   * @param {String} className
+   * @param {String} toggleClassName
    */
-  toggleCssClass(btn, className) {
+  toggleCssClass(btn, toggleClassName) {
     if (!btn) {
-      throw new Error('toggleCssClass: Button not passed to parameters')
+      throw new Error('toggleCssClass: button not passed to parameters')
     }
-    if (typeof className !== 'string') {
+
+    if (typeof toggleClassName !== 'string') {
       throw new Error(
         'toggleCssClass: className is not passed or is not a string'
       )
     }
-    btn.classList.toggle(className)
+
+    btn.classList.toggle(toggleClassName)
+    btn.blur()
+  },
+
+  /**
+   * add/remove css class of specified elements
+   * @param {HTMLButtonElement} btn
+   * @param {String} nodeClassName
+   * @param {String} toggleClassName
+   */
+  toggleCssClassNodes(btn, nodeClassName, toggleClassName) {
+    if (!btn) {
+      throw new Error('toggleCssClassNodes: button not passed to parameters')
+    }
+
+    if (
+      typeof nodeClassName !== 'string' ||
+      typeof toggleClassName !== 'string'
+    ) {
+      throw new Error(
+        'toggleCssClassNodes: nodeClassName or toggleClassName are not passed or is not a string'
+      )
+    }
+    console.log('test')
+
+    const nodes = document.querySelectorAll(`.${nodeClassName}`)
+    console.log(nodes)
+
+    if (nodes.length === 0) {
+      throw new Error(
+        'toggleCssClassNodes: nodes with class classNameNode do not exist'
+      )
+    }
+
+    nodes.forEach((node) => node.classList.toggle(toggleClassName))
+    btn.blur()
+  },
+
+  /**
+   * add/remove attribute hidden for nodes with className
+   * @param {HTMLButtonElement} btn
+   * @param {String} nodeClassName
+   */
+  toggleVisibleNodes(btn, nodeClassName) {
+    if (!btn) {
+      throw new Error('toggleVisibleNodes: button not passed to parameters')
+    }
+
+    if (typeof nodeClassName !== 'string') {
+      throw new Error(
+        'toggleVisibleNodes: classNameNode is not passed or is not a string'
+      )
+    }
+
+    const nodes = document.querySelectorAll(`.${nodeClassName}`)
+
+    if (nodes.length === 0) {
+      throw new Error(
+        'toggleVisibleNodes: nodes with class classNameNode do not exist'
+      )
+    }
+
+    nodes.forEach((node) => {
+      if (node.hasAttribute('hidden')) {
+        node.removeAttribute('hidden')
+      } else {
+        node.setAttribute('hidden', 'hidden')
+      }
+    })
     btn.blur()
   }
 }
